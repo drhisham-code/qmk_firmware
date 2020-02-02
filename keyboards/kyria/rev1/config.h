76ebfd7a6337c629fdb0c0307e2239b03dcd9014
Drashna Jael're <drashna@live.com>
2020-02-01 23:40:52 -0800

    [Keyboard] Make Kyria more Configurator friendly

--- keyboards/kyria/rev1/config.h
+++ keyboards/kyria/rev1/config.h
@@ -17,7 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x0000
@@ -55,9 +54,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define SOFT_SERIAL_PIN D2
 
-#define RGB_DI_PIN D3
-#define RGBLED_SPLIT { 10, 10 }
-#define RGBLED_NUM 20
+#ifdef RGBLIGHT_ENABLE
+#    define RGB_DI_PIN D3
+#    define RGBLED_SPLIT { 10, 10 }
+#    define RGBLED_NUM 20
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#endif
+
+#ifdef OLED_DRIVER_ENABLE
+#    define OLED_DISPLAY_128X64
+#endif
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
